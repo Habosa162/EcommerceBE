cbb3ad394d0e56e3f3532b273ef8f81a90778e17
TokaMahmoud <67740450+TokaMahmoud@users.noreply.github.com>
2025-03-28 00:16:32 +0200

    order

--- Application/Services/OrderService.cs
+++ Application/Services/OrderService.cs
@@ -1,9 +1,21 @@
 ﻿using ECommerce.Application.Interfaces;
+using ECommerce.Domain.Models;
+using ECommerce.Infrastructure.Repositories;
 
 namespace ECommerce.Application.Services
 {
     public class OrderService : IOrderService
     {
+        private readonly OrderRepository _OrderRepository;
+        public OrderService(OrderRepository orderRepository)
+        {
+            _OrderRepository = orderRepository;
+        }
+
+        public async Task CreateOrder(Order order)
+        {
+            await _OrderRepository.setOrder(order);
+        }
 
     }
 }
