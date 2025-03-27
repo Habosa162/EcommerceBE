584713efbf84aa86bcbf12f60f5b836b91aa8a1d
Habosa162 <habosa2000@hotmail.com>
2025-03-27 06:15:00 +0200

    addingApplication

---
+++ Application/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using EComm.API.DTOs;
+using EComm.Domain.Models;
+
+namespace EComm.Application.Interfaces
+{
+    public interface IAuthService
+    {
+        public bool IsAuthenticated { get; }
+        public string? GenerateJwtToken(AppUser user); 
+        public Task<string> login(string email, string password);
+        public Task<bool> logout();
+        public Task<string> register(RegisterDTO registerDTO);
+    }
+}
